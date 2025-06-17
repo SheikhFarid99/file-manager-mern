ddda98cea0cbb12624b95fc7de4135fe03f7cf5c
sfarid99 <sheikhfarid498@gmail.com>
2025-06-17 05:51:08 -0600

    sidebar update

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,27 +2,67 @@ import React from 'react'
 import { FcOpenedFolder } from 'react-icons/fc'
 import { IoIosArrowForward } from 'react-icons/io'
 
-const Sidebar = ({ folderItems,type }) => {
+const Sidebar = ({
+    folderItems,
+    type,
+    setElement,
+    setPath,
+    element,
+    setElementIds,
+    storageSize,
+    elementIds,
+    setSelectItem
+}) => {
+
+
+    const toggleFolder = (id) => {
+
+        const check = elementIds.find(e => e === id)
+
+        if (check) {
+            const temp = elementIds.filter(e => e !== id)
+            setElementIds(temp)
+        } else {
+            setElementIds([...elementIds, id])
+        }
+    }
+
+    const viewFolder = (item) => {
+
+        setSelectItem([])
+        setElement(item.path)
+        setPath(item.path)
+
+    }
+
 
 
     const Folder = ({ name, items, id, path, type }) => {
-        return <div className='overflow-hidden transition-all duration-500'>
-            <div className='h-[40px] absolute hover:bg-slate-200 w-full left-0'></div>
+        return <div
+            id={id}
+            style={{
+                height: elementIds.includes(id) ? 'auto' : '40px',
+                paddingLeft: '12px',
+                display: 'block',
+                position: 'relative'
+            }}
+            className='overflow-hidden transition-all duration-500'>
+            <div className={`h-[40px] absolute hover:bg-slate-200 w-full left-0 ${element === path ? 'bg-blue-500' : ''}`}></div>
             <li className='flex w-full justify-center items-center relative h-full'>
-                <div >
+                <div className={` ${element === path ? 'text-white' : 'text-black'} font-normal ${items?.length > 0 ? 'pl-[12px]' : 'pl-[30px]'} pr-[8px] flex justify-start items-center gap-[8px] w-full cursor-pointer relative  `} >
                     {
-                        items?.length > 0 && <div>
-                            <span><IoIosArrowForward /></span>
+                        items?.length > 0 && <div className={`${elementIds.includes(id) ? 'rotate-90' : 'rotate-0'} absolute `}>
+                            <span onClick={() => toggleFolder(id)} className='text-xs'><IoIosArrowForward /></span>
                         </div>
                     }
-                    <div>
+                    <div onClick={() => viewFolder({ path, id, type, name })} className={` w-full flex justify-start items-center gap-x-1 ${items?.length > 0 ? 'pl-[18px]' : ''} `}>
                         <span><FcOpenedFolder /></span>
                         <span>{name}</span>
                     </div>
                 </div>
             </li>
             {
-                items && items.map((item) => (
+                items && items.length > 0 && items.map((item) => (
                     <Item key={item.id} {...item} />
                 ))
             }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,6 +10,22 @@ const Home = () => {
   const { store } = useAuthContext()
   const [lists, setLists] = useState([])
 
+  const [element, setElement] = useState("")
+  const [elementIds, setElementIds] = useState([])
+
+  const [path, setPath] = useState('')
+  const [viewType, setViewType] = useState('grid')
+
+  const [selectItem, setSelectItem] = useState([])
+
+  const [folderName, setFolderName] = useState('')
+
+  const [copyData, setCopyData] = useState([])
+
+  const [moveType, setMoveType] = useState('')
+
+  const [storageSize, setStorageSize] = useState(0)
+
   const config = {
     headers: {
       Authorization: `Bearer ${store.token}`
@@ -31,6 +47,7 @@ const Home = () => {
       getFiles()
     }
   }, [])
+
   return (
     <div className='w-screen h-screen flex justify-center items-center bg-gray-200'>
       <div className='w-[70vw] bg-white shadow-lg h-[90vh] relative overflow-hidden'>
@@ -41,7 +58,17 @@ const Home = () => {
           <div className='w-full h-[calc(100%-45px)] min-w-[500px]'>
             <div className='w-[300px] border-l border-r h-full relative'>
               {
-                lists && lists.length > 0 && <Sidebar folderItems={lists} />
+                lists && lists.length > 0 && <Sidebar
+                  folderItems={lists}
+                  setElement={setElement}
+                  setPath={setPath}
+                  path={path}
+                  element={element}
+                  setElementIds={setElementIds}
+                  storageSize={storageSize}
+                  elementIds={elementIds}
+                  setSelectItem={setSelectItem}
+                />
               }
             </div>
           </div>
